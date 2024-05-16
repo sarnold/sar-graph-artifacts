f05c640a8a6bc2269b5f9597a4c8ba19a5f16771
Steve Arnold <sarnold@vctlabs.com>
2024-05-16 10:19:15 -0700

    chg: extract data subsets for individual graph tests
    * sketch out basic utilization plots for cpu and memory
    
    Signed-off-by: Steve Arnold <sarnold@vctlabs.com>

--- scripts/cpu.py
+++ scripts/cpu.py
@@ -1,78 +1,67 @@
 #!/usr/bin/env python3
 """
-Author        :Julio Sanz
-Website       :www.elarraydejota.com
-Email         :juliojosesb@gmail.com
-Description   :Generate CPU graph from cpu.dat file
-Dependencies  :Python 3.x, matplotlib
-Usage         :python cpu.py
-License       :GPLv3
+Simple CPU utilization plot.
 """
 
+import csv
+from datetime import datetime
+
+import matplotlib.dates
+import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import csv
-from datetime import datetime
-import matplotlib.dates
 
-# ======================
-# VARIABLES
-# ======================
 
-# Aesthetic parameters
 plt.rcParams.update({'font.size': 8})
 plt.rcParams['lines.linewidth'] = 1.5
 time_format = matplotlib.dates.DateFormatter('%H:%M:%S')
 plt.gca().xaxis.set_major_formatter(time_format)
 plt.gcf().autofmt_xdate()
 
-# Time (column 0)
 x = []
-# Data arrays
-cpu_usage = []
-idle_cpu = []
+cpu_percent_usage = []
 
-# ======================
-# FUNCTIONS
-# ======================
 
 def generate_graph():
-    with open('data/sa09-cpu.csv', 'r') as csvfile:
+    with open('data/sa12-cpu.csv', 'r') as csvfile:
         data_source = csv.reader(csvfile, delimiter=';')
         hostname = None
+        prev_total = 0
+        prev_idle = 0
         for row in data_source:
-            if 'CPU' in row[3]:
+            # hostname;interval;timestamp;CPU;%usr;%nice;%sys;%iowait;%steal;%irq;%soft;%guest;%gnice;%idle
+            if 'CPU' in row[3] or 'RESTART' in row[3]:
+                continue
+            if row[0].startswith('#'):
                 continue
             if hostname is None:
                 hostname = row[0]
-            # [2] column is date-time
-            # Convert to datetime data type
             a = datetime.strptime((row[2].split(' ')[1]),'%H:%M:%S')
             x.append((a))
-            # The remaining columns contain data
-            cpu_usage.append(row[4] + row[6])
-            idle_cpu.append(row[13])
+            # CPU usage needs more calculation using deltas
+            # idle = %idle + %iowait
+            cpu_idle = float(row[7]) + float(row[13])
+            # non_idle = %user + %nice + %system + %irq + %softirq + %steal
+            non_idle = float(row[4]) + float(row[5]) + float(row[6]) + float(row[9]) + float(row[10]) + float(row[8])
+            cpu_total = cpu_idle + non_idle
+            cpu_delta = cpu_total - prev_total
+            idle_delta = cpu_idle - prev_idle
+            cpu_use = cpu_delta - idle_delta
+            cpu_usage = (100 * cpu_use / cpu_delta)
+            cpu_percent_usage.append(cpu_usage)
 
-    # Plot lines
-    plt.plot(x,cpu_usage, label='User %', color='r', antialiased=True)
-    plt.plot(x,idle_cpu, label='Idle %', color='g', antialiased=True)
+    plt.plot(x, np.asarray(cpu_percent_usage, float), label='% Used', color='g', antialiased=True)
 
-    # Graph properties
     plt.xlabel('Time',fontstyle='italic')
-    plt.ylabel('CPU %',fontstyle='italic')
-    plt.title(f'{hostname} CPU usage graph')
+    plt.ylabel('CPU used (%)',fontstyle='italic')
+    plt.title(f'{hostname} CPU utilization')
     plt.grid(linewidth=0.4, antialiased=True)
     plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15), ncol=2, fancybox=True, shadow=True)
     plt.autoscale(True)
 
-    # Graph saved to PNG file
     plt.savefig('out/cpu.png', bbox_inches='tight')
-    #plt.show()
 
-# ======================
-# MAIN
-# ======================
 
 if __name__ == '__main__':
     generate_graph()
--- scripts/ram.py
+++ scripts/ram.py
@@ -1,85 +1,56 @@
 #!/usr/bin/env python3
 """
-Author        :Julio Sanz
-Website       :www.elarraydejota.com
-Email         :juliojosesb@gmail.com
-Description   :Generate RAM graph from ram.dat file
-Dependencies  :Python 3.x, matplotlib
-Usage         :python ram.py
-License       :GPLv3
+Simple memory utilization plot (data as float).
 """
 
+import csv
+from datetime import datetime
+
+import matplotlib.dates
+import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import csv
-from datetime import datetime
-import matplotlib.dates
 
-# ======================
-# VARIABLES
-# ======================
 
-# Aesthetic parameters
 plt.rcParams.update({'font.size': 8})
 plt.rcParams['lines.linewidth'] = 1.5
 time_format = matplotlib.dates.DateFormatter('%H:%M:%S')
 plt.gca().xaxis.set_major_formatter(time_format)
 plt.gcf().autofmt_xdate()
 
-# Time (column 0)
 x = []
-# Memory data arrays
-free_mem = []
-used_mem = []
-buffer_mem = []
-cached_mem = []
+mem_used = []
 
-# ======================
-# FUNCTIONS
-# ======================
 
 def generate_graph():
-    with open('data/sa09-ram.csv', 'r') as csvfile:
+    with open('data/sa12-ram.csv', 'r') as csvfile:
         data_source = csv.reader(csvfile, delimiter=';')
         hostname = None
         for row in data_source:
+            # hostname;interval;timestamp;kbmemfree;kbavail;kbmemused;%memused;kbbuffers;kbcached ...
+            if 'CPU' in row[3] or 'RESTART' in row[3]:
+                continue
             if row[0].startswith('#'):
                 continue
             if hostname is None:
                 hostname = row[0]
-            # [2] column is date-time
-            # Convert to datetime data type
             a = datetime.strptime((row[2].split(' ')[1]),'%H:%M:%S')
             x.append((a))
-            # The remaining columns contain data
-            # 3:kbmemfree 5:kbmemused 7:kbbuffers 8:kbcached
-            free_mem.append((int(row[3])/1024)+(int(row[7])/1024)+(int(row[8])/1024))
-            used_mem.append((int(row[5])/1024))
-            buffer_mem.append(int(row[7])/1024)
-            cached_mem.append(int(row[8])/1024)
+            # %memused
+            mem_used.append(row[6])
 
-    # Plot lines
-    plt.plot(x,free_mem, label='Free', color='g', antialiased=True)
-    plt.plot(x,used_mem, label='Used', color='r', antialiased=True)
-    plt.plot(x,buffer_mem, label='Buffer', color='b', antialiased=True)
-    plt.plot(x,cached_mem, label='Cached', color='c', antialiased=True)
+    plt.plot(x, np.asarray(mem_used, float), label='% Used', color='g', antialiased=True)
 
-    # Graph properties
     plt.xlabel('Time',fontstyle='italic')
-    plt.ylabel('Memory (MB)',fontstyle='italic')
-    plt.title(f'{hostname} RAM usage graph')
+    plt.ylabel('RAM used (%)',fontstyle='italic')
+    plt.title(f'{hostname} Memory utilization')
     plt.grid(linewidth=0.4, antialiased=True)
     plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15), ncol=2, fancybox=True, shadow=True)
     plt.autoscale(True)
 
-    # Graph saved to PNG file
     plt.savefig('out/ram.png', bbox_inches='tight')
-    #plt.show()
 
-# ======================
-# MAIN
-# ======================
 
 if __name__ == '__main__':
     generate_graph()
